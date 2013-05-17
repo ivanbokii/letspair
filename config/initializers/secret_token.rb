6d0d375e8a463aa1a1028c01eaa6b010682f104f
ivanbokii <bokiyis@gmail.com>
2013-05-17 18:15:10 +0300

    create welcome page with design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letspair::Application.config.secret_token = '4d5d29d8404b5519de4dd4cc7a1c954ff07dd39b487b6b62883b205889754248eb467b28fd441822cd116890739bc7e8deace57b4cc45982aa8bc4f6f75c9480'
