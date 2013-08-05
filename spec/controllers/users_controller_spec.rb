849ff186de9281cc62c16f28cb3b52185e9bf6e6
Anders Lundström <anders@urkraft.se>
2013-08-05 20:40:32 +0200

    user_controller_spec reflects our changes for user list

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,17 @@
 require'spec_helper' 
 
 describe UsersController do
-  it "returns list of all users" do
-    user = FactoryGirl.create(:user)
-    get :index
-    expect(assigns(:users)).to match_array [user]
+  it "get_users renders json with users" do
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+
+    get :get_users
+    expect(response.body).to eq(User.all.to_json)
+    # expect(response.body).to match_array [user]
   end
-  
-  it "renders the :index users view" do
+
+  it "renders the :index view" do
     get :index
     expect(response).to render_template :index
   end
-
-  it "returns the list of all users in the right order"
 end
